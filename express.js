e06e2a1eb97690c7694308bf17d0b9dcda6643ec
lq5793 <3348814747@qq.com>
2025-05-13 08:44:43 +0800

    创建一个名为express.js的文件
    利用express.js框架实现一个web服务器

---
+++ express.js
@@ -0,0 +1,20 @@
+/*
+*Net.Server创建服务器
+*/
+// 1.加载net核心模块
+var net = require('net');
+// 2.创建一个服务器应用程序，得到一个服务器实例对象
+var server = net.createServer();
+
+// 3.监听客户端的连接事件connection，连接成功就会执行回调处理函数
+server.on('connection',function(){
+	console.log('有客户端连接上来了');
+});
+// 5.服务器有一个事件叫做listening，表示开启监听成功之后回调处理函数
+server.on('listening',function(){
+	console.log('服务器开启监听成功了，正在等待客户端连接');
+});
+
+// 4.启动服务器，开启监听
+// 监听127.0.0.1 
+server.listen(3000,'127.0.0.1');
